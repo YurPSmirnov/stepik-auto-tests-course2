ddee83468ac9d2b00b252b2ca776c7262dda442f
YurPSmirnov <Yur.P.Smirnov@gmail.com>
2022-01-22 12:35:10 +0300

    Добавил файл с программой

---
+++ lesson7_step6.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time, math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try: 
+    link = "http://suninjuly.github.io/get_attribute.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    x=browser.find_element_by_id("treasure").get_attribute('valuex')
+    print(x)
+    y=calc(x)
+    print(y)
+    input1=browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    
+    option2 = browser.find_element_by_id("robotsRule")
+    option2.click()
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+    
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
